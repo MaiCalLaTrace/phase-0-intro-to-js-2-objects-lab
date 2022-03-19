73e7e4aced40660125b874ec53fc6ff63c0d45ec
maicallatrace <maicallatrace@gmail.com>
2022-03-18 21:58:57 -0600

    REPO

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Sam' ,
+    streetAdress: '11 Broadway'
+}
+
+function updateEmployeeWithKeyAndValue(employee) {
+    return {name: 'Sam' , streetAddress: '11 Broadway'};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee) {
+    employee.name = 'Sam'
+    employee.streetAddress = '12 Broadway'
+    return (employee)
+}
+
+function  deleteFromEmployeeByKey(employee) {
+    const newEmployee = {employee};
+    delete newEmployee.name;
+    return (newEmployee)
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee) {
+    delete employee.name;
+    return employee
+}
